69e4e722dd80477d493d7bf44a45be3a41d07e72
ToterFisch <miproz@web.de>
2026-04-22 21:52:51 +0200

    Create boot.asm

---
+++ boot.asm
@@ -0,0 +1,12 @@
+[org 0x7c00]
+
+mov ah, 0x0e
+mov al 'H'
+int 0x10
+
+jmp $
+
+times 510-($-$$) db 0
+dw 0xaa55
+nasm -f bin boot.asm -o boot.bin
+qemu-system-x86_64 -drive format=raw,file=boot.bin
